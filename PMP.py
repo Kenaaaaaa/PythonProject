f18014b6fca6c9298c6d7fd87d5815a2817fba20
Kenaaaaaa <kenatirana@yahoo.com>
2025-04-02 17:17:39 +0200

    OutFun

--- PMP.py
+++ PMP.py
@@ -1,11 +1,14 @@
 
-
-a=int(input("Jep nr1"))
-b=int(input("Jep nr2"))
-mbetja=a%b
-while mbetja>0:
-    a=b
-    b=mbetja
+def pmp(a,b):
     mbetja=a%b
-print(b)
+    while mbetja>0:
+        a=b
+        b=mbetja
+        mbetja=a%b
+    return b
+nr1 = int(input("Jep nr1"))
+nr2 = int(input("Jep nr2"))
+print("PMP =",pmp(nr1,nr2))
+
+
 
