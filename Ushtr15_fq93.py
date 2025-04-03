420167d8e9f68df907b0127bdc0bc94e57c147e8
Kenaaaaaa <kenatirana@yahoo.com>
2025-04-04 01:22:35 +0200

    iThjeshte

---
+++ Ushtr15_fq93.py
@@ -0,0 +1,15 @@
+#i , j dhe k
+i=7
+j=3
+k=5
+if i<j:
+    if j<k:
+        i=j
+    else:
+        i=k
+else:
+    if j>k:
+        j=i
+    else:
+        i=k
+print("i= ",i, "j = ",j, "k= ",k)
---
+++ kontrollo.py
@@ -0,0 +1,18 @@
+dita_anglisht=input("Fut nje dite na anglisht").lower()
+
+if dita_anglisht == "monday":
+    print("E hene")
+elif dita_anglisht == "tuesday":
+    print("E Marte")
+elif dita_anglisht == "wednesday":
+    print("E Merkure")
+elif dita_anglisht == "thursday":
+    print("E Enjte")
+elif dita_anglisht == "friday":
+    print("E Premte")
+elif dita_anglisht == "saturday":
+    print("E Shtune")
+elif dita_anglisht == "sunday":
+    print("E Diel")
+else:
+    print("Fjala nuk eshte valide")
