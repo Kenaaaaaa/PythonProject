2444a5de296bfab94690cfa3aa75e107ac65235c
kenaaaaaaa <kenatirana@yahoo.com>
2025-05-07 14:14:43 +0200

    Kapitulli_7_Ushtrimi_10.py

---
+++ Kapitulli_7_Ushtrimi_10.py
@@ -0,0 +1,45 @@
+#Shkruani listen qe paraqesin secila prej shprehjeve te meposhtme
+# a) [8] * 4
+[8] * 4
+
+"""Kjo shprehje shumzon listën [8] me 4, që do të thotë e përsërit përmbajtjen e saj katër herë.
+Rezultati: [8, 8, 8, 8]
+"""
+
+# b) 6 * [2, 7 ]
+
+"""Kjo shumzon listën [2, 7] me 6, që do të thotë: e përsërit përmbajtjen e listës 6 herë radhazi.
+Rezultati: [2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7]
+
+"""
+
+
+# c) [1, 2, 2] + [ 'a', 'b', 'c', 'd']
+
+"""Kjo bashkon dy lista duke i vendosur njëra pas tjetrës. Operatorin + në Python, kur përdoret me lista, krijon një listë të re që përmban të gjithë elementët e të dyjave.
+Rezultati: [1, 2, 2, 'a', 'b', 'c', 'd']
+
+"""
+
+
+#d) 3 * [1, 2] + [4, 2]
+
+"""Ajo përbëhet nga dy pjesë:
+
+3 * [1, 2] → përsërit listën [1, 2] tri herë → rezulton: [1, 2, 1, 2, 1, 2]
+
+Pastaj i shtohet lista [4, 2] duke përdorur +, që bashkon listat.
+Rezultati: [1, 2, 1, 2, 1, 2, 4, 2]
+"""
+
+#e) 3 * ([1,2] + [4,2])
+"""Bashkimi i dy listave:
+[1, 2] + [4, 2] → rezulton në:[1, 2, 4, 2]
+Përsëritja e listës së bashkuar 3 herë:
+
+3 * [1, 2, 4, 2] → [1, 2, 4, 2, 1, 2, 4, 2, 1, 2, 4, 2]
+
+Rezultati: [1, 2, 4, 2, 1, 2, 4, 2, 1, 2, 4, 2]
+
+
+"""
