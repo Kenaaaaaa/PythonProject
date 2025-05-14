ded0d81eb77eeeae889115e1c4d6cc231db6a7b5
kenaaaaaaa <kenatirana@yahoo.com>
2025-05-14 19:07:30 +0200

    Kapitulli_10_Ushtrimi_2.py_Ushtrimi_7.py

---
+++ Kapitulli_10_Ushtrimi_2.py
@@ -0,0 +1,9 @@
+"""hkruani një program për të numëruar sasinë e karaktereve në string
+(Mos përdorni metodën e paracaktuar len)"""
+
+stringu = input("Shkruani një string: ")
+numerues = 0
+for karakter in stringu:
+    numerues += 1
+
+print("Numri i karaktereve është:", numerues)
