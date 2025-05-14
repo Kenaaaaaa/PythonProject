20188dc588b95f22a9f1d969f7fb6974fc106c3c
kenaaaaaaa <kenatirana@yahoo.com>
2025-05-14 15:37:58 +0200

    Kapitulli_8_Ushtrimi_8.py

---
+++ Kapitulli_8_Ushtrimi_8.py
@@ -0,0 +1,12 @@
+"""A janë elementët në fjalor të renditur apo të pa renditur?
+
+"""
+
+#ZGJIDHJA
+
+"""
+Në versionet e vjetra të Python (deri në Python 3.6), fjalorët ishin të parenditur.
+Në Python 3.7 e lart, fjalorët ruajnë rendin e shtimit të çelësave, por teknikisht ende konsiderohen si të parenditur nga pikëpamja logjike (sepse rendi nuk është i garantuar për logjikën e programimit si te një listë e zakonshme).
+
+Përgjigje e saktë:
+Elementët e fjalorit janë të parenditur (ose jo të renditur në mënyrë të qëllimshme)."""
