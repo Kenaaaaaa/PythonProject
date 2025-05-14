00ce13f9405f1afb559ee683d2c16941cfbf13bd
kenaaaaaaa <kenatirana@yahoo.com>
2025-05-14 19:21:43 +0200

    Kapitulli_8_Ushtrimi_8

--- Kapitulli_8_Ushtrimi_8.py
+++ Kapitulli_8_Ushtrimi_8.py
@@ -8,5 +8,5 @@
 Në versionet e vjetra të Python (deri në Python 3.6), fjalorët ishin të parenditur.
 Në Python 3.7 e lart, fjalorët ruajnë rendin e shtimit të çelësave, por teknikisht ende konsiderohen si të parenditur nga pikëpamja logjike (sepse rendi nuk është i garantuar për logjikën e programimit si te një listë e zakonshme).
 
-Përgjigje e saktë:
+Përgjigje e sakte
 Elementët e fjalorit janë të parenditur (ose jo të renditur në mënyrë të qëllimshme)."""
